7af331ca358ef0d434f1583fdc90dc0991657be9
Allan Donato <81997690+AllanDonato7@users.noreply.github.com>
2021-06-30 13:27:48 -0300

    Delete ex_04.py

--- Mundo 01/ex_04.py
+++
@@ -1,9 +0,0 @@
-n=input("Digite Algo  : ")
-print ("o tipo primitivo desse valor é " , type(n))
-print ("só tem espaços ?", n.isspace())
-print ("é numerico ?" , n.isnumeric())
-print ("é alfabetico ?" , n.isalpha())
-
-
-
-
