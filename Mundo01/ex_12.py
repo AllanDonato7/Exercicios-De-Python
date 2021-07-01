c802c4cae6e66adda19fbd9f941bda6015433e0a
Allan Donato <81997690+AllanDonato7@users.noreply.github.com>
2021-07-01 13:34:25 -0300

    Delete ex_12.py

--- Mundo01/ex_12.py
+++
@@ -1,3 +0,0 @@
-preço = float(input("Qual é o valor do produto ? R$"))
-novo = preço - (preço * 5 / 100)
-print ("O preço do produto era {: .2f} mas com desconto de 5 % ficara {: .2f}" .format(preço, novo))
