473bff82d6857b2e1afd3cf66946656ef77d4f8f
Allan Donato <81997690+AllanDonato7@users.noreply.github.com>
2021-07-01 13:34:08 -0300

    Delete ex _13.py

--- Mundo01/ex _13.py
+++
@@ -1,5 +0,0 @@
-#  Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-
-salario = float(input ("Qual é o salario do funcionario ? R$"))
-novo = salario - (salario * 20 / 100)
-print ("Um funcionario que ganhava R${: .2f} , com 20 % de baixa , começara a ganha R${: .2f}" .format(salario, novo))
