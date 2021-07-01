964427e500e64f8445e0b53c019e7ba753f4fdd4
Allan Donato <81997690+AllanDonato7@users.noreply.github.com>
2021-07-01 13:34:39 -0300

    Delete ex_11.py

--- Mundo01/ex_11.py
+++
@@ -1,6 +0,0 @@
-larg = float(input(" Largura da Parede : "))
-alt = float(input(" Altura da Parede : "))
-area = larg * alt
-print ("Sua Parede tem a dimensao de {}x{} e sua area é de {}m2" .format(larg, alt, area))
-tinta = area / 2
-print ("Sua parede ira precisar de {}L de tinta " .format(tinta))
